26aa5790b90f9ffb7d873b028cd167a15c041711
coca-create <himotoki.sansu@gmail.com>
2024-06-30 14:18:45 +0900

    Add files via upload

--- moz/moz1_func.py
+++ moz/moz1_func.py
@@ -169,4 +169,35 @@ def transcribe_audio(input_folder, output_folder):
     # Memory cleanup
 torch.cuda.empty_cache()
 
+# ウィジェットの作成
+input_folder_widget = widgets.Text(
+    value='/content/drive/MyDrive/My Input',
+    placeholder='Enter input folder path',
+    description='Input Folder:',
+    disabled=False
+)
 
+output_folder_widget = widgets.Text(
+    value='/content/drive/MyDrive/My Output',
+    placeholder='Enter output folder path',
+    description='Output Folder:',
+    disabled=False
+)
+
+transcribe_button = widgets.Button(description="Transcribe")
+
+# ボタンがクリックされたときの処理
+def on_button_click(b):
+    input_folder = input_folder_widget.value
+    output_folder = output_folder_widget.value
+    result_files = transcribe_audio(input_folder, output_folder)
+    print("Output files:")
+    for file_path in result_files:
+        print(file_path)
+
+def clk():
+    
+    return transcribe_button.on_click(on_button_click)
+
+def dp():
+    return display(input_folder_widget),display(output_folder_widget),display(transcribe_button)
